1ecce8613e4142089c6c3359214efe220202a7c6
Thomas <thomas.gourouza@gmail.com>
2022-11-13 18:12:22 +0100

    message

--- components/pages/page1.php
+++ components/pages/page1.php
@@ -1,4 +1,16 @@
 <h2>page1</h2>
-<?php 
-require_once "./../links.php";
-?>
+<h2> les liens </h2>
+<ul>
+	<?php include "links.html"; ?>
+</ul>
+<div>
+	<p> Soumettez vos liens !!!</p>
+	<form name="form" id="form1" method="post" action="addlink.php">
+		<label>lien</label>
+		<input type="text" name="lien" value="http://" />
+		<label>commentaire du lien</label>
+		<input type="text" name="commentaire" />
+		<input type="hidden" name="css" value="<?php echo $css; ?>"/>
+		<input name="submit" value="soumettre" type="submit" />
+	</form>
+</div>
