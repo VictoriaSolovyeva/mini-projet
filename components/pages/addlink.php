119e0433f7a963a3854e6ca565e4f47a940d5aef
Thomas <thomas.gourouza@gmail.com>
2022-11-13 19:19:54 +0100

    test add link

--- components/pages/addlink.php
+++ components/pages/addlink.php
@@ -4,24 +4,24 @@ $commentaire=$_REQUEST["commentaire"];
 $css=$_REQUEST["css"];
 $fich="links.html";
 
-if (!file_exists($fich)) {
-	touch($fich);
-}
+// if (!file_exists($fich)) {
+// 	touch($fich);
+// }
 
-if ($f = fopen($fich, "a")) {
-	$test = @fopen($lien, 'r');
-	if ($test) {
-		//le lien est valide, on l'insère en fin de fichier
-		$code='<li><a href="'.$lien.'"><b>'.$commentaire.'</b> : '.$lien.'</a></li>'; //construction du code à insérer
-		fputs($f, $code); //écriture en fin de fichier
-	} else {
-		echo 'lien non valide : '.$lien;
-	}
-	fclose($f);
-}
+// if ($f = fopen($fich, "a")) {
+// 	$test = @fopen($lien, 'r');
+// 	if ($test) {
+// 		//le lien est valide, on l'insère en fin de fichier
+// 		$code='<li><a href="'.$lien.'"><b>'.$commentaire.'</b> : '.$lien.'</a></li>'; //construction du code à insérer
+// 		fputs($f, $code); //écriture en fin de fichier
+// 	} else {
+// 		echo 'lien non valide : '.$lien;
+// 	}
+// 	fclose($f);
+// }
 ?>
 
-<form name="form" id="form" method="post" action="index2.php">
+<form name="form" id="form" method="post" action="index.php">
 	<input name="css" type="hidden" value="<?php echo $css; ?>">
 	<input name="page" type="hidden" value="page1">
 	<input name="titre" type="hidden" value="apropos">
