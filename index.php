4c62f78ffa333f44bffa6fc528b0e83be827d3ae
Thomas <thomas.gourouza@gmail.com>
2022-11-14 00:33:29 +0100

    big update

--- index.php
+++ index.php
@@ -21,18 +21,25 @@
 	<meta charset="utf-8"/>
 	<title>Titre du site</title>
 	<link rel="icon" href="images/fox.png">
-	<?php echo '<link href="css/'.$css.'.css"'. ' rel="stylesheet" type="text/css">'; ?>
+	<link href="settings.css" rel="stylesheet" type="text/css">
+	<link href="index.css" rel="stylesheet" type="text/css">
+	<?php echo '<link href="themes/'.$css.'.css"'. ' rel="stylesheet" type="text/css">'; ?>
 </head>
 <body>
-	<div class="header">
-		<?php include "components/header.php"; ?>
+	<div class="header custom-row">
+		<?php include "components/header/header.php"; ?>
 	</div>
-	<div class="content">
-		<?php include "components/pages/".$page.".php"; ?>
+	<div class="body custom-row">
+		<div class="side-bar">
+			side bar
+		</div>
+		<div class="content">
+			<?php include "components/pages/".$page."/".$page.".php"; ?>
+		</div>
 	</div>
 	<div class="footer">
-		<?php include "components/footer.php"; ?>
+		<?php include "components/footer/footer.php"; ?>
 	</div>
-	<script src="js/script.js"></script>
+	<script src="index.js"></script>
 </body>
 </html>
