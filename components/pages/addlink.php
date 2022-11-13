941b322805539b4d6fc480ac9f843a5986143375
Thomas <thomas.gourouza@gmail.com>
2022-11-13 19:09:29 +0100

    test add link

--- components/pages/addlink.php
+++ components/pages/addlink.php
@@ -21,7 +21,7 @@ if ($f = fopen($fich, "a")) {
 }
 ?>
 
-<form name="form" id="form" method="post" action="./../../index.php">
+<form name="form" id="form" method="post" action="page1.php">
 	<input name="css" type="hidden" value="<?php echo $css; ?>">
 	<input name="page" type="hidden" value="page1">
 	<input name="titre" type="hidden" value="apropos">
