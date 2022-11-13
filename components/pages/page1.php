66ef130316a0c464d4dc49ce42495a7e5533bcea
Thomas <thomas.gourouza@gmail.com>
2022-11-13 21:22:34 +0100

    test add link

--- components/pages/page1.php
+++ components/pages/page1.php
@@ -8,9 +8,9 @@
 	<?php
 	echo '<form name="form" id="form1" method="post" action="addlink.php">
 			<label>lien</label>
-			<input type="text" name="lien" value="http://" value="'.($valid !== 'true' ? $lien : 'http://').'" '.($valid !== 'true' ? 'style="color: red;"' : '').' />
+			<input type="text" name="lien" value="http://" value="'.($valid === 'false' ? $lien : 'http://').'" '.($valid === 'false' ? 'style="color: red;"' : '').' />
 			<label>commentaire du lien</label>
-			<input type="text" name="commentaire" value="'.($valid !== 'true' ? $commentaire : '').'" '.($valid !== 'true' ? 'style="color: red;"' : '').' />
+			<input type="text" name="commentaire" value="'.($valid === 'false' ? $commentaire : '').'" '.($valid === 'false' ? 'style="color: red;"' : '').' />
 			<input type="hidden" name="css" value="'.$css.'"/>
 			<input type="submit" name="submit" value="soumettre"/>
 		</form>';
