5caa188039be5ed5f91f1e2c296fd2de2ca8cd1f
Thomas <thomas.gourouza@gmail.com>
2022-11-13 19:49:39 +0100

    test add link

--- components/pages/addlink.php
+++ components/pages/addlink.php
@@ -1,28 +1,7 @@
-<?php
-$lien=$_REQUEST["lien"];
-$commentaire=$_REQUEST["commentaire"];
-$css=$_REQUEST["css"];
-$fich="links.html";
 
-if (!file_exists($fich)) {
-	touch($fich);
-}
-
-if ($f = fopen($fich, "a")) {
-	$test = @fopen($lien, 'r');
-	if ($test) {
-		//le lien est valide, on l'insère en fin de fichier
-		$code='<li><a href="'.$lien.'"><b>'.$commentaire.'</b> : '.$lien.'</a></li>'; //construction du code à insérer
-		fputs($f, $code); //écriture en fin de fichier
-	} else {
-		echo 'lien non valide : '.$lien;
-	}
-	fclose($f);
-}
-?>
 
 <form name="form" method="post" action="/index.php">
-	<input name="css" type="hidden" value="<?php echo $css; ?>">
+	<input name="css" type="hidden" value="css0">
 	<input name="page" type="hidden" value="page1">
 	<input name="titre" type="hidden" value="apropos">
 	<input id="submit" type="submit"/>
