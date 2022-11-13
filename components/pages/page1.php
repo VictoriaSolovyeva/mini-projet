093d276c4de3b37c056cec070bd5502da961bec9
Thomas <thomas.gourouza@gmail.com>
2022-11-13 22:50:46 +0100

    improve form

--- components/pages/page1.php
+++ components/pages/page1.php
@@ -1,7 +1,3 @@
-<?php
-	$invalidLinkMessage='<span style="color: red;"> Lien non valide</span>';
-	$message=$invalidLinkMessage;
-?>
 <h2>page1</h2>
 <h2> les liens </h2>
 <ul>
@@ -19,20 +15,20 @@
 		'.($valid === 'false' ? 'value="'.$commentaire.'"' : '').' />
 		<input type="hidden" name="css" value="'.$css.'"/>
 		<input type="submit" name="submit" value="soumettre"/>
-		'.($valid === 'false' ? $message : '').'
-	</form>
-	
-	<script type="text/javascript">
-		function update(element) {
-			const value = element.value;
-			if (value === '.$lien.') {
-				element.style.color = "red";
-				'.$message=$invalidLinkMessage.';
-			} else {
-				element.style.color = "black";
-				'.($message="").';
-			}
-		}
-	</script>';
+		'.($valid === 'false' ? '<span id="invalidMessage" style="color: red;"> Lien non valide</span>' : '').'
+	</form>';
 	?>
 </div>
+<script type='text/javascript'>
+	function update(element) {
+		const value = element.value;
+		const invalidMessage = document.getElementById('invalidMessage');
+		if (value === <?php $lien ?>) {
+			element.style.color = 'red';
+			invalidMessage.style.visibility = 'visible';
+		} else {
+			element.style.color = 'black';
+			invalidMessage.style.visibility = 'hidden';
+		}
+	}
+</script>
