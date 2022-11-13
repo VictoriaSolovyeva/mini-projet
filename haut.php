7419b5073b19967759ed774ef7b1965ec2a004ce
ThomasGourouza <thomas.gourouza@gmail.com>
2022-11-13 12:55:21 +0000

    change title

--- haut.php
+++ haut.php
@@ -1,3 +1,3 @@
 <header>
-	<h1 class="titre">Architecture d'un site dynamique : <? echo $titre; ?> </h1>
+	<h1 class="titre">Exemple d'architecture d'un site dynamique : <? echo $titre; ?> </h1>
 </header>
