a650265379dde463fd63d7b410d3167b9e0e01a6
Thomas <thomas.gourouza@gmail.com>
2022-11-13 22:24:12 +0100

    test add link

--- components/pages/page1.php
+++ components/pages/page1.php
@@ -6,14 +6,23 @@
 <div>
 	<p> Soumettez vos liens !!!</p>
 	<?php
-	echo ($valid === 'false' ? '<p> lien non valide</p>' : '').'
-	<form name="form" id="form1" method="post" action="addlink.php">
-			<label>lien</label>
-			<input type="text" name="lien" '.($valid === 'false' ? 'value="'.$lien.'" style="color: red;"' : 'value="http://"').' />
-			<label>commentaire du lien</label>
-			<input type="text" name="commentaire" value="'.($valid === 'false' ? $commentaire : '').'" '.($valid === 'false' ? 'style="color: red;"' : '').' />
-			<input type="hidden" name="css" value="'.$css.'"/>
-			<input type="submit" name="submit" value="soumettre"/>
-		</form>';
+	'<form name="form" id="form1" method="post" action="addlink.php">
+		<label>lien</label>
+		<input type="text" name="lien" id="link"
+		'.($valid === 'false' ? 'value="'.$lien.'" style="color: red;" onkeypress="update(this)"' : 'value="http://"').' />
+		<label>commentaire du lien</label>
+		<input type="text" name="commentaire"
+		'.($valid === 'false' ? 'value="'.$commentaire.'"' : '').' />
+		<input type="hidden" name="css" value="'.$css.'"/>
+		<input type="submit" name="submit" value="soumettre"/>
+		'.($valid === 'false' ? '<span style="color: red;">Lien non valide</span>' : '').'
+	</form>';
 	?>
 </div>
+<script type='text/javascript'>
+	function update(element) {
+		element.style.color = 'black';
+		const value = element.value;
+		console.log(value);
+	}
+</script>
