23417f4db11565bb53f35fe8a12a666f5e002b28
Thomas <thomas.gourouza@gmail.com>
2022-11-14 19:34:48 +0100

    update

--- components/header/header.php
+++ components/header/header.php
@@ -7,21 +7,21 @@
 	</div>
 	<div class="nav-box">
 		<nav>
-			<span>
+			<span class="nav-item">
 				<?php echo '<a href="index.php?page=home&css='.$css.'">Accueil</a>'; ?>
-			</span>&nbsp;
-			<span>
+			</span>
+			<span class="nav-item">
 				<?php echo '<a href="index.php?page=page1&css='.$css.'">Page 1</a>'; ?>
-			</span>&nbsp;
-			<span>
+			</span>
+			<span class="nav-item">
 				<?php echo '<a href="index.php?page=page2&css='.$css.'">Page 2</a>'; ?>
-			</span>&nbsp;
-			<span>
+			</span>
+			<span class="nav-item">
 				<?php echo '<a href="index.php?page=page3&css='.$css.'">Page 3</a>'; ?>
-			</span>&nbsp;
-			<span>
+			</span>
+			<span class="nav-item">
 				<?php echo '<a href="index.php?page=page4&css='.$css.'">Page 4</a>'; ?>
-			</span>&nbsp;
+			</span>
 		</nav>
 	</div>
 </div>
