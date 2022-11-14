6a9a1260f071ae807d8aea64917b77bb0407052d
Thomas <thomas.gourouza@gmail.com>
2022-11-14 01:09:54 +0100

    update

--- components/header/header.php
+++ components/header/header.php
@@ -1,12 +1,27 @@
 <header>
-	<h1 class="titre">Exemple d'architecture d'un site dynamique</h1>
+	<h1>Exemple d'architecture d'un site dynamique</h1>
 </header>
 <nav>
-	<?php
-		echo '<p><a href="index.php?page=home&css='.$css.'">Accueil</a></p>';
-		echo '<p><a href="index.php?page=page1&css='.$css.'">Page 1</a></p>';
-		echo '<p><a href="index.php?page=page2&css='.$css.'">Page 2</a></p>';
-		echo '<p><a href="index.php?page=page3&css='.$css.'">Page 3</a></p>';
-		echo '<p><a href="index.php?page=page4&css='.$css.'">Page 4</a></p>';
-	?>
+<div class="box">
+    <div class="logo-box">
+        <div class="logo"></div>
+    </div>
+    <div class="tabs-box">
+        <div class="tab cliquable">
+			<?php echo '<p><a href="index.php?page=home&css='.$css.'">Accueil</a></p>'; ?>
+        </div>
+        <div class="tab cliquable">
+			<?php echo '<p><a href="index.php?page=page1&css='.$css.'">Page 1</a></p>'; ?>
+        </div>
+        <div class="tab cliquable">
+			<?php echo '<p><a href="index.php?page=page2&css='.$css.'">Page 2</a></p>'; ?>
+        </div>
+        <div class="tab cliquable">
+			<?php echo '<p><a href="index.php?page=page3&css='.$css.'">Page 3</a></p>'; ?>
+        </div>
+        <div class="tab cliquable">
+			<?php echo '<p><a href="index.php?page=page4&css='.$css.'">Page 4</a></p>'; ?>
+        </div>
+    </div>
+</div>
 </nav>
