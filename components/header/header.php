234e7ef52a78f74cdba157c078ec8582a22e9489
Thomas <thomas.gourouza@gmail.com>
2022-11-14 19:28:34 +0100

    update

--- components/header/header.php
+++ components/header/header.php
@@ -1,25 +1,25 @@
 <div class="row">
-	<div class="block1">
-		<img src="images/fox.png" alt="logo" class="logo tab">
+	<div class="logo-box">
+		<img src="images/fox.png" alt="logo" class="logo">
 	</div>
-	<div class="block2">
+	<div class="title-box">
 		<h1><?php echo $title; ?></h1>
 	</div>
-	<div class="block3">
+	<div class="nav-box">
 		<nav>
-			<span class="tab cliquable">
+			<span>
 				<?php echo '<a href="index.php?page=home&css='.$css.'">Accueil</a>'; ?>
 			</span>&nbsp;
-			<span class="tab cliquable">
+			<span>
 				<?php echo '<a href="index.php?page=page1&css='.$css.'">Page 1</a>'; ?>
 			</span>&nbsp;
-			<span class="tab cliquable">
+			<span>
 				<?php echo '<a href="index.php?page=page2&css='.$css.'">Page 2</a>'; ?>
 			</span>&nbsp;
-			<span class="tab cliquable">
+			<span>
 				<?php echo '<a href="index.php?page=page3&css='.$css.'">Page 3</a>'; ?>
 			</span>&nbsp;
-			<span class="tab cliquable">
+			<span>
 				<?php echo '<a href="index.php?page=page4&css='.$css.'">Page 4</a>'; ?>
 			</span>&nbsp;
 		</nav>
