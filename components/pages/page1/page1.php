0aa3f1576928cfb793e81fc4382d5eb8c97e0b11
Thomas Gourouza <thomas.gourouza@de.markant.com>
2022-11-14 16:37:18 +0100

    update

--- components/pages/page1/page1.php
+++ components/pages/page1/page1.php
@@ -5,21 +5,27 @@
 </ul>
 <div>
 	<p> Soumettez vos liens !!!</p>
-	<?php echo
-	'<form name="form" method="post" action="services/addlink.php">
+	<form name="form" method="post" action="services/addlink.php">
 		<label>lien</label>
 		<input type="text" name="lien"
-		'.($valid === 'false' ?
-		'value="'.$lien.'" style="color: red;" onkeyup="update(this, \''.$lien.'\')"' : 'value="http://"').' />
+			<?php echo $valid === 'false' ?
+				'value="'.$lien.'" style="color: red;" onkeyup="update(this, \''.$lien.'\')"'
+				: 'value="http://"';
+			?>
+		/>
 		<label>commentaire du lien</label>
 		<input type="text" name="commentaire"
-		'.($valid === 'false' ? 'value="'.$commentaire.'"' : '').' />
+			<?php echo $valid === 'false' ?
+				'value="'.$commentaire.'"' : '';
+			?>
+		/>
 		<input type="hidden" name="css" value="'.$css.'"/>
 		<input type="submit" name="submit" id="submit" value="soumettre"
-		'.($valid === 'false' ? 'disabled' : '').'
+			<?php echo $valid === 'false' ? 'disabled' : ''; ?>
 		/>
-		'.($valid === 'false' ? '<span id="invalidMessage" style="color: red;"> Lien non valide</span>' : '').'
-	</form>';
-	?>
+		<?php echo $valid === 'false' ?
+			'<span id="invalidMessage" style="color: red;">Lien non valide</span>' : '';
+		?>
+	</form>
 </div>
 <script src="components/pages/page1/page1.js"></script>
