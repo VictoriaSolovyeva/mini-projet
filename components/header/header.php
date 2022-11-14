e2c3b4074225243e83b08c5cd6fe84e21074d5f2
Thomas Gourouza <thomas.gourouza@de.markant.com>
2022-11-14 17:31:13 +0100

    update

--- components/header/header.php
+++ components/header/header.php
@@ -1,21 +1,23 @@
 <div class="tabs-box">
 	<img src="images/fox.png" alt="logo" class="logo tab">
 	<span class="tab">Exemple d'architecture d'un site dynamique</span>&nbsp;
-	<nav class="tab">
-		<span class="tab cliquable">
-			<?php echo '<a href="index.php?page=home&css='.$css.'">Accueil</a>'; ?>
-		</span>&nbsp;
-		<span class="tab cliquable">
-			<?php echo '<a href="index.php?page=page1&css='.$css.'">Page 1</a>'; ?>
-		</span>&nbsp;
-		<span class="tab cliquable">
-			<?php echo '<a href="index.php?page=page2&css='.$css.'">Page 2</a>'; ?>
-		</span>&nbsp;
-		<span class="tab cliquable">
-			<?php echo '<a href="index.php?page=page3&css='.$css.'">Page 3</a>'; ?>
-		</span>&nbsp;
-		<span class="tab cliquable">
-			<?php echo '<a href="index.php?page=page4&css='.$css.'">Page 4</a>'; ?>
-		</span>&nbsp;
-	</nav>
+	<div class="tab">
+		<nav>
+			<span class="tab cliquable">
+				<?php echo '<a href="index.php?page=home&css='.$css.'">Accueil</a>'; ?>
+			</span>&nbsp;
+			<span class="tab cliquable">
+				<?php echo '<a href="index.php?page=page1&css='.$css.'">Page 1</a>'; ?>
+			</span>&nbsp;
+			<span class="tab cliquable">
+				<?php echo '<a href="index.php?page=page2&css='.$css.'">Page 2</a>'; ?>
+			</span>&nbsp;
+			<span class="tab cliquable">
+				<?php echo '<a href="index.php?page=page3&css='.$css.'">Page 3</a>'; ?>
+			</span>&nbsp;
+			<span class="tab cliquable">
+				<?php echo '<a href="index.php?page=page4&css='.$css.'">Page 4</a>'; ?>
+			</span>&nbsp;
+		</nav>
+	</div>
 </div>
