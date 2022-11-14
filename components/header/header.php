9b885044cbbe142c5fdb65c50fc520cc1e317b33
Thomas <thomas.gourouza@gmail.com>
2022-11-14 01:28:27 +0100

    update

--- components/header/header.php
+++ components/header/header.php
@@ -1,25 +1,22 @@
 <div class="box">
     <div class="logo-box">
-        <div class="logo"></div>
-		<div>
-			Exemple d'architecture d'un site dynamique
-		</div>
+		Exemple d'architecture d'un site dynamique
     </div>
     <div class="tabs-box">
-        <div class="tab cliquable">
+        <span class="tab cliquable">
 			<?php echo '<p><a href="index.php?page=home&css='.$css.'">Accueil</a></p>'; ?>
-        </div>
-        <div class="tab cliquable">
+        </span>
+        <span class="tab cliquable">
 			<?php echo '<p><a href="index.php?page=page1&css='.$css.'">Page 1</a></p>'; ?>
-        </div>
-        <div class="tab cliquable">
+        </span>
+        <span class="tab cliquable">
 			<?php echo '<p><a href="index.php?page=page2&css='.$css.'">Page 2</a></p>'; ?>
-        </div>
-        <div class="tab cliquable">
+        </span>
+        <span class="tab cliquable">
 			<?php echo '<p><a href="index.php?page=page3&css='.$css.'">Page 3</a></p>'; ?>
-        </div>
-        <div class="tab cliquable">
+        </span>
+        <span class="tab cliquable">
 			<?php echo '<p><a href="index.php?page=page4&css='.$css.'">Page 4</a></p>'; ?>
-        </div>
+        </span>
     </div>
 </div>
