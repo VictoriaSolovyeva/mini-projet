f34932ea64aa1d338000e04f0bcc6060bca60199
ThomasGourouza <thomas.gourouza@gmail.com>
2023-01-15 20:26:13 +0000

    update

--- components/pages/accueil.php
+++ components/pages/accueil.php
@@ -1,12 +1,12 @@
+<div class="content">
+    <h1>
+    Bienvenue sur le site de Vika et Cynthia!
+    </h1>
+    <p>
+        Tout d'abord, choisisez le thème qui vous convient le mieux pour explorer le site. Vous pouvez garder le style actuel ou bien vous rendre dans l'univers de Patrick, Squidward ou Crabs :)
+    </p>
+</div>
 
-
-<h1>
-Bienvenue sur le site de Vika et Cynthia!
-
-</h1>
-<p>
-    Tout d'abord, choisisez le thème qui vous convient le mieux pour explorer le site. Vous pouvez garder le style actuel ou bien vous rendre dans l'univers de Patrick, Squidward ou Crabs :)
-</p>
 
 <?php 
     if($css == 'bob')
