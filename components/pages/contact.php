7d71c285f1993e659325cd44e113525f27424474
Thomas Gourouza <thomas.gourouza@de.markant.com>
2023-01-24 20:01:00 +0100

    update

--- components/pages/contact.php
+++ components/pages/contact.php
@@ -1,12 +1,18 @@
 <h1>Contact</h1>
-<p>
-    <ul>
-        <li>
-            Victoriya Solovyeva: mail: mail@unistra.com
-        </li>
-        <li>
-            Cynthia: mail: mail@unistra.com
-        </li>
 
-    </ul>
-</p>
+<div class="row">
+    <div class="column contact-container">
+        <div class="contact vika">
+        </div>
+        <p>
+            Victoriya Solovyeva: mail: mail@unistra.com
+        </p>
+    </div>
+    <div class="column contact-container">
+        <div class="contact cynthia">
+        </div>
+        <p>
+            Cynthia: mail: mail@unistra.com
+        </p>
+    </div>
+</div>
