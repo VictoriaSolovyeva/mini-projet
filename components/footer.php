72647f14e209610cdd8b5e5095c71ec2c8055c99
Thomas Gourouza <thomas.gourouza@de.markant.com>
2023-01-29 13:17:48 +0100

    end

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,23 @@
+<?php $activeLink = 'class="active-link"'; ?>
+
 <div>
-	<a href="index.php?page=<?php echo $page ?>&css=bob">Bob</a>
-	<a href="index.php?page=<?php echo $page ?>&css=patrick">Patrick</a>
-	<a href="index.php?page=<?php echo $page ?>&css=print">CSS print</a>
+	<a
+		href="index.php?page=<?php echo $page ?>&css=bob"
+		<?php if ($css=="bob") { echo $activeLink; } ?>
+	>
+		Bob
+	</a>
+	<a
+		href="index.php?page=<?php echo $page ?>&css=patrick"
+		<?php if ($css=="patrick") { echo $activeLink; } ?>
+	>
+		Patrick
+	</a>
+	<a
+		href="index.php?page=<?php echo $page ?>&css=print"
+		<?php if ($css=="print") { echo $activeLink; } ?>
+	>
+		CSS print
+	</a>
 	<a href="index.php?page=<?php echo $page ?>&css=">CSS vide</a>
 </div>
